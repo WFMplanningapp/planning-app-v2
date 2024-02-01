d22b176c76c81493b2482997aa604bc28cf6a8fe
headauto <102734032+headauto@users.noreply.github.com>
2024-02-01 12:08:43 +0000

    Update actual.js

--- pages/api/data/entries/actual.js
+++ pages/api/data/entries/actual.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
 				data: null,
 			})
 		}
-		if (!(verification.verified && verification.permission <= 2)) {
+		if (!(verification.verified && verification.permission <= 4)) {
 			console.log(verification.verified, verification.permission)
 			return res.status(401).json(verification)
 		}
